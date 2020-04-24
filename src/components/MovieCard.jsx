8d622c4de3f51359069ccaf6e70a530ab113722c
Hfreitas <hfreitas86@gmail.com>
2020-04-24 18:15:25 -0300

    alterado componente MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,14 +6,16 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-      <div className="movie-card movie-card-body">
-        <div>
-          <img src={imagePath} alt={title} className="movie-card-image" />
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <div>
+            <img src={imagePath} alt={title} className="movie-card-image" />
+          </div>
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={rating} />
         </div>
-        <h4 className="movie-card-title">{title}</h4>
-        <h5 className="movie-card-subtitle">{subtitle}</h5>
-        <p className="movie-card-storyline">{storyline}</p>
-        <Rating rating={rating} />
       </div>
     );
   }
