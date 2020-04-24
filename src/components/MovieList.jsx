1fa97653669355b21b07571a3dee818c6e739d3f
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-04-24 16:29:16 -0300

    testbot

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((list) => <MovieCard key={list.title} movie={list} />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
