b5fcd1f2d5174262415ac1b2e6295caa00d3ba3d
Thiago Tavares <thiagooo15@gmail.com>
2020-04-24 15:32:14 -0300

    adicionando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
 // implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+			<header>
+				<h1>Movie Cards Library L</h1>
+			</header>
+    );
+  }
+}
+
+export default Header;
