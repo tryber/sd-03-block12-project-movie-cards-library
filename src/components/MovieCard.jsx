818d7e7adf50e83e1207eaf20560f19506086dcc
fabemiliano <fab.emiliano@gmail.com>
2020-04-24 15:50:09 -0300

    requisito 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,9 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (<header><h1>Movie Cards Library</h1></header>);
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,21 +1,12 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>
-        {movies.map((movie) => (
-          <div key={movie.title}>
-            <div>{movie.title}</div>
-            <div>{movie.subtitle}</div>
-            <div>{movie.storyline}</div>
-            <div>{movie.rating}</div>
-            <img src={movie.imagePath} alt="foto do jogo" />
-          </div>
-        ))}
-      </div>
+      movies.map((movie) => <MovieCard movie={movie} key={movie.title} />)
     );
   }
 }
