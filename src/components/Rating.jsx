fe1112ded433aaba741d4c93fdd719f7b24ec851
Cardoso <lsc.cardosoo@gmail.com>
2020-04-24 18:08:07 -0300

    test'

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 
 class Rating extends React.Component {
   render() {
     return (
-      <div className='rating'>
-      </div>
+      <div className="rating" />
     );
   }
 }
