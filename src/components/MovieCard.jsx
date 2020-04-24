b52643619f7c404ea22868345bf7f0e03522cbad
nato-x <alvares.renato@hotmail.com>
2020-04-24 15:30:07 -0300

    Movie card add movie after props const, what is Must use destructuring props assignment?

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,14 +5,12 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const movie = this.props;
-    console.log(movie);
-    const { title, subtitle, storyline, rating, imagePath } = movie;
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     console.log(rating);
     return (
       <div className="movie-card">
         <div className="movie-card-body">
-          <img className="movie-card-image" src={imagePath} alt={`Movie ${movie.title}`} />
+          <img className="movie-card-image" src={imagePath} alt={`Movie ${title}`} />
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
