3c8ca8681ada7323d7d905f9e2ac4f64b2ce6093
leonardodamasceno8 <leonardodamasceno8@users>
2020-04-24 15:33:52 -0300

    alguns requisitos almoust done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+    render() {
+        const { title, subtitle, storyline, rating, imagePath} = this.props.movies;
+
+        return 0;
+    }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from "react";
+import MovieCard from './MovieCard'
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <div className="movie-List">
+        {movies.map(movie => <MovieCard key={movies.title} movies={movies} />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
