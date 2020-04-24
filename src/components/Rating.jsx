d3078d70100b94d69b50a8eeb3382254c0f40ad0
Marco Barbosa <marco.meireles.b@gmail.com>
2020-04-24 17:40:20 -0300

    codeclimate

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
-      return (
-        <div>{rating}</div>
-      );
+    return (
+      <div>{rating}</div>
+    );
   }
 }
 
