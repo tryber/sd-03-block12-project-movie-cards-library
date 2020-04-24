76640d0bbf8e33954049d957ed8d72eaaa939588
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-04-24 16:53:24 -0300

    Adicionando componentes e lógica de renderização

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import movies from './data';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
-    <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+    <div className='App'>
+    <Header />
+    <MovieList movies={movies} />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,29 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.cards;
+    return (
+      <div className='movie-card'>
+        <img className='movie-card-image' src={imagePath} alt='test' />
+        <div className='movie-card-body'>
+          <h4 className='movie-card-title'>{title}</h4>
+          <h5 className='movie-card-subtitle'>{subtitle}</h5>
+          <p className='movie-card-storyline'>{storyline}</p>
+        </div>
+        <footer className='movie-card-rating'>
+          <p className='rating'>{rating}</p>
+        </footer>
+      </div>
+    );
+  }
+}
+
+// 6 Crie um componente chamado MovieCard
+// Esse componente representa um cartão de filme.
+// MovieCard deve receber uma prop movie. Essa prop será um objeto,
+// contendo as propriedades,
+// title, subtitle, storyline, imagePath e rating.
+
+export default MovieCard;
