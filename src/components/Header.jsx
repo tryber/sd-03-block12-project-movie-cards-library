7eed6e0609700c202c9a298a943fe6ff17e355a7
Wenderson Belko <contato@wendersonbelko.com>
2020-04-24 14:12:30 -0300

    implementação do heander no app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component here
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <header>This is my Header!</header>
+      </>
+    );
+  }
+}
+
+export default Header;
