9ec1ae8e75b3de0f73f9e7b1961f92b74f6ff3b3
Rodrigo Brezolin <brezolinr@gmail.com>
2020-04-24 16:03:33 -0300

    funfou

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,25 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movieCards;
+
+    return (
+
+      <div className='movie-card'>
+        <div className='movie-card-body'>
+          <img src={imagePath} className='movie-card-image' alt={title} /> 
+          <h3 className="movie-card-title"> {title} </h3>
+          <h3 className="movie-card-subtitle"> {subtitle} </h3>
+          <p className="movie-card-storyline"> {storyline} </p>
+          <Rating className="movie-card-rating" rating={rating} />
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+
+  render() {
+    
+    const movies = this.props.movieList;
+
+    return (
+      <div className='movie-list'>
+        { movies.map((element) => <MovieCard key={element.title} movieCards={element} />) }
+      </div>
+    );
+    
+  }
+
+}
+
+export default MovieList;
