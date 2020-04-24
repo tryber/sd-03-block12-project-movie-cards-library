ddc5b6862c4e32ef7652cc33ec03765e73ecd31e
Cardoso <lsc.cardosoo@gmail.com>
2020-04-24 17:44:19 -0300

    adicionar tags de titulo, subtitulo, e rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
+    const { title, subtitle, storyline} = movie;
     return (
-      <img src={movie.imagePath} alt="Cartaz do Filme" />
+      <div>
+        <img src={movie.imagePath} alt="Cartaz do Filme" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </div>
     );
   }
 }
