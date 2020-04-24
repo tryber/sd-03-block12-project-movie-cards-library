46d7c6fe4602b5174a2da35af0f226eeed506931
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-04-24 17:23:53 -0300

    Projeto finalizado sem css devidamente feito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,24 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+	render() {
+		const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+
+		return (
+			<li className="movie-card">
+				<div className="movie-card-body">
+					<img src={imagePath} alt={title} className="movie-card-image" />
+					<h4 className="movie-card-title">{title}</h4>
+					<h5 className="movie-card-subtitle">{subtitle}</h5>
+					<p className="movie-card-storyline">{storyline}</p>
+					<div className="movie-card-rating">
+						<Rating rating={rating} />
+					</div>
+				</div>
+			</li>
+		);
+	}
+}
+
+export default MovieCard;
