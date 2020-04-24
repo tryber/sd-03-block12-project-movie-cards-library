1d4dd0037d4dbe49f6716ce29ae56a155cc9aa69
Cardoso <lsc.cardosoo@gmail.com>
2020-04-24 18:02:31 -0300

    adicionar rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-
+import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline } = movie;
+    const { title, subtitle, storyline, rating } = movie;
     return (
       <div>
         <img src={movie.imagePath} alt="Cartaz do Filme" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <Rating rating={rating} />
       </div>
     );
   }
