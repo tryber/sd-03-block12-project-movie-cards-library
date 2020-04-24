49a935be5da178ba86b49f05145c576df7bf14a3
Luma Arruda <luma.oarruda@gmail.com>
2020-04-24 16:31:14 -0300

    code climate corrections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-     <Header/>
+      < Header />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 class Header extends React.Component {
-  render () {
+  render() {
     return (
-    <header className='movie-card-header'>
-      <h1>Movie Cards Library</h1>
-    </header>
-    )
+      <header className="movie-card-header">
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
   }
 }
 
