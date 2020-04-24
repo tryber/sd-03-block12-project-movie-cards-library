790ac62071fa95d8bae2e62191766374c1efe707
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-04-24 14:25:42 -0300

    first commit with some changes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+import React from 'react';
+import MovieList from './MovieList';
+
+export default function MovieCard() {
+  const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+  return (
+    <>
+      <img src={imagePath} alt={title} />
+      <h4>
+        {title}
+      </h4>
+      
+    </>
+  );
+}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,9 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+export default function MovieList() {
+  const { movies } = this.props.movies;
+  return (
+    movies.map((movie) => <MovieCard key={movie.title} />)
+  );
+}
