6cd0bdf81cf106878c55457fde20c8ded540b0d4
Cardoso <lsc.cardosoo@gmail.com>
2020-04-24 16:16:06 -0300

    Consertando cc do commit anterior

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+
 class Header extends React.Component {
-  render () {
+  render() {
     return (
       <header>
         <h1>Movie Cards Library</h1>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <MovieList movies={movies} />
+    );
+  }
+}
+
+export default MovieList;
