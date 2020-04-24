927bc19038738827ddba35a5fe68d582211e2355
fabemiliano <fab.emiliano@gmail.com>
2020-04-24 15:35:49 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Data from './data';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={Data} />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie) => (
+          <div key={movie.title}>
+            <div>{movie.title}</div>
+            <div>{movie.subtitle}</div>
+            <div>{movie.storyline}</div>
+            <div>{movie.rating}</div>
+            <img src={movie.imagePath} alt="foto do jogo" />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
