1ffd43751aeea0434a66df6c26265f8232424998
luizfernando <luizfernandotic@gmail.com>
2020-04-24 16:34:47 -0300

    adicionando MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title , subtitle, storyline, imagePath } = this.props.movie;
+    return (
+      <div className="moviecard">
+        <div>
+          <img src={imagePath} alt={`${title} poster`}></img>
+          <p> {title} </p>
+          <p> {subtitle} </p>
+          <p> {storyline} </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
