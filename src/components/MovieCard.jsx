2e3388992fcc72ddd4b61e3ca62c80e85c5266ef
Cardoso <lsc.cardosoo@gmail.com>
2020-04-24 16:38:03 -0300

    test

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,1 @@
-import React from 'react';
-
-class MovieList extends React.Component {
-  render() {
-    return (
-      <MovieList movies={movies} />
-    );
-  }
-}
-
-export default MovieList;
+// implement MovieCard component here
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React from 'react';
+import movies from '../data.js';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <MovieList movies={movies} />
+    );
+  }
+}
+
+export default MovieList;
