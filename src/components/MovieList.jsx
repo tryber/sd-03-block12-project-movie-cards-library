7c3d380497a916fe2545b7d467c3fc4b6073c5cb
rogerfaustino <roger_faustino@hotmail.com>
2020-04-24 18:27:48 -0300

    App and MovieList components

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+// Este componente representará toda a área com os cartões de filmes.
+// MovieList deve receber uma prop movies, que é um array de objetos com informações de um filme.
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        {movies.map((m) => (
+          <MovieCard key={m.imagePath} movie={m} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
