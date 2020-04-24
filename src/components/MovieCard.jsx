f75dec7f94e4c57d90019967f6ae1ca2ec3e41dc
Hfreitas <hfreitas86@gmail.com>
2020-04-24 15:28:19 -0300

    implementado componente moviecard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,2 +1,18 @@
 // implement MovieCard component here
 import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath } = this.props.movies;
+    return (
+      <div>
+        <p>Title: {title}</p>
+        <p>Subtitle: {subtitle}</p>
+        <p>Storyline: {storyline}</p>
+        <img src={imagePath} alt={title} />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
