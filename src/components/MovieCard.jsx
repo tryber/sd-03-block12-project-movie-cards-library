0b288ab5f519fb2492488d343c35926355b285a5
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-04-24 17:35:10 -0300

    Projeto finalizado sem css devidamente feito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,15 +6,15 @@ class MovieCard extends React.Component {
 		const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
 
 		return (
-			<li className="movie-card">
-				<div className="movie-card-body">
-					<img src={imagePath} alt={title} className="movie-card-image" />
-					<h4 className="movie-card-title">{title}</h4>
-					<h5 className="movie-card-subtitle">{subtitle}</h5>
-					<p className="movie-card-storyline">{storyline}</p>
-					<div className="movie-card-rating">
+			<li className = "movie-card">
+				<div className = "movie-card-body">
+					<img src = {imagePath} alt = {title} className = "movie-card-image" />
+					<h4 className = "movie-card-title">{title}</h4>
+					<h5 className = "movie-card-subtitle">{subtitle}</h5>
+					<p className = "movie-card-storyline">{storyline}</p>
+					<p className = "movie-card-rating">
 						<Rating rating={rating} />
-					</div>
+					</p>
 				</div>
 			</li>
 		);
