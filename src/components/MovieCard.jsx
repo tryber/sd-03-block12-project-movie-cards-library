5b7d22f86042c2b4e6fe30b719541a59e13a649b
Thiago Tavares <thiagooo15@gmail.com>
2020-04-24 17:38:48 -0300

    codeclimate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,11 +6,11 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, imagePath } = this.props.movie;
 
     return (
-      <div className='MovieCard'>
+      <div className="MovieCard">
         <div>
           <img src={imagePath} alt={`${title} poster`} />
-          <p> {title} </p>
-          <p> {subtitle} </p>
+          <h4> {title} </h4>
+          <h5> {subtitle} </h5>
           <p> {storyline} </p>
         </div>
       </div>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ class MovieList extends React.Component {
     const movies = this.props.movies;
     return (
       <div className="movielist">
-        {movies.map(movie => <MovieCard key={movie.title} movie={movie} />)}
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
   }
