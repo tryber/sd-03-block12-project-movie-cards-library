4ad1275a3fe9ea9883cd1786590fd95afaf27159
rogerfaustino <roger_faustino@hotmail.com>
2020-04-24 14:46:31 -0300

    Starting the movie cards library project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,9 @@
 // implement Header component here
+import React from 'react';
+class Header extends React.Component {
+  render() {
+    return <h1>Starting the movie cards library project, {this.props.} {this.props.}</h1>;
+  }
+}
+
+export default Header;
