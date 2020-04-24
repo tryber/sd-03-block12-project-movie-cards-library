dacc9d963255bd13a833a569bf3e8698cc21babc
luldelrey <rpgstrikered@gmail.com>
2020-04-24 16:14:33 -0300

    Project done

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,18 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+
+    return (
+      <span className="movie-card-rating">
+        <p className="rating">
+          {rating}
+        </p>
+      </span>
+    );
+  }
+}
+
+export default Rating;
