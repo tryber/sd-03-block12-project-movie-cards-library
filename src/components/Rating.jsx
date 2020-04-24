a526e261231bca389fd44e5b33376e15d28e76f9
Lourenço <loulage@gmail.com>
2020-04-24 16:50:33 -0300

    All test passed o/

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 
 class Rating extends React.Component {
     render() {
+        const { rating } = this.props;
         return (
             <div className="rating">
-                <p className="movie-card-rating">{this.props}</p>
+                <span className="movie-card-rating">{rating}</span>
             </div>    
         )
     }
