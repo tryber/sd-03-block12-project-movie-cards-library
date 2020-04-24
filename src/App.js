19669b0869b861709c9814a608cb95b0ea742b83
Luma Arruda <luma.oarruda@gmail.com>
2020-04-24 18:00:30 -0300

    mudar pastas e adicionar movie-card-body

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import MovieList from './MovieList';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
 import movieData from './data';
 
 function App() {
