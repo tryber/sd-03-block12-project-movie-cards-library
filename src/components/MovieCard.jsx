473ba7cd1252da484767405cc9788a43a80741c4
Hfreitas <hfreitas86@gmail.com>
2020-04-24 16:43:35 -0300

    implementado css inicial

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,13 +6,13 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-      <div>
+      <div className="movie-card movie-card-body">
         <div>
-          <img src={imagePath} alt={title} />
+          <img src={imagePath} alt={title} className="movie-card-image" />
         </div>
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
         <Rating rating={rating} />
       </div>
     );
