4d0498b103faf92a8e8de4f322ba86baa09c60a2
Clayton Guimaraes Leite Pereira <claytonpereira@mtprev.local>
2020-04-24 12:30:01 -0400

    Salvando requisito 3 do projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import dataMovies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        {dataMovies.map((element) => <MovieCard key={element.title} />)}
+
+      </div>
+    );
+  }
+}
+
+export default MovieList;
