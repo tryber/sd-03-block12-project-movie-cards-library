40f4730229eb916bf720582c358ad3c6badc92d4
Marco Barbosa <marco.meireles.b@gmail.com>
2020-04-24 20:16:00 -0300

    Consertando a imagem de Ghost in the Shell

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,14 +7,16 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <div>
-        <div className="movie-card"><img src={imagePath} alt={title} /><br />
-          <div><h4>{title}</h4>
-            <h5>{subtitle}</h5>
-            <p>{storyline}</p>
+      <div className="movie-card">
+        <div className="movie-card-body"><img className="movie-card-image" src={imagePath} alt={title} /><br />
+          <div><h4 className="movie-card-title">{title}</h4>
+            <h5 className="movie-card-subtitle">{subtitle}</h5>
+            <p className="movie-card-storyline">{storyline}</p>
           </div>
         </div>
-        <Rating rating={rating} />
+        <div className="movie-card-rating">
+          <Rating rating={rating} />
+        </div>
       </div>
     );
   }
