628b6eea9d837f0d87bd6a57341c86a308270a72
Clayton Pereira <cpereiramt@gmail.com>
2020-04-24 18:23:51 -0400

    Salvando as alteraçoes do projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -15,8 +15,9 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
-          <Rating className="movie-card-rating" rating={rating} />
         </div>
+        <div className="movie-card-rating"><Rating rating={rating} /></div>
+        
       </div>
     );
   }
