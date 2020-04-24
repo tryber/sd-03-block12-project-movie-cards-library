dad386b81331cf38e33dc7017b10011e85da32fc
Marco Barbosa <marco.meireles.b@gmail.com>
2020-04-24 20:18:56 -0300

    codeclimate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,8 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div className="movie-card">
-        <div className="movie-card-body"><img className="movie-card-image" src={imagePath} alt={title} /><br />
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={imagePath} alt={title} /><br />
           <div><h4 className="movie-card-title">{title}</h4>
             <h5 className="movie-card-subtitle">{subtitle}</h5>
             <p className="movie-card-storyline">{storyline}</p>
