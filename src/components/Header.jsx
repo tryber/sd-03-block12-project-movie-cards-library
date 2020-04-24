fa7630164a5dbc0e8a7eef463cec3b2e4d577ea0
Gabriel Lucas <dreadpool38@gmail.com>
2020-04-24 15:25:45 -0300

    Some Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 export default class Header extends Component {
-    render() {
-        return (
-            <div />
-        );
-    }
+  render() {
+    return (
+      <header className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>
+      </header>
+
+    );
+  }
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,24 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import Rating from './Rating';
+
+export default class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    console.log(movie);
+    return (
+      <div className="movie-card ">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={movie.imagePath} alt="film description" />
+          <h4 className="movie-card-title">{movie.title}</h4>
+          <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
+          <p className="movie-card-storyline">{movie.storyline}</p>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={movie.rating} />
+        </div>
+      </div>
+
+
+    );
+  }
+}
