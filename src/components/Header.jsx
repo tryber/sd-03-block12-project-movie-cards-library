1dbe201c98d8028dab8d090caa70199ebbdde381
henrique <hbaetaleite@gmail.com>
2020-04-24 19:30:43 -0300

    Iniciando projeto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
 // implement Header component here
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
