78b8e2df674b11aa3d26286578e09ecf2caa811d
DiegoRafaelAF <draf0314@gmail.com>
2020-04-24 20:19:19 -0300

    Correção Sintaxe

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,18 +3,18 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
 
     return (
-      <div calssName="movie-card">
+      <div className="movie-card">
         <div className="movie-card-body">
           <img className="movie-card-image" src={imagePath} alt={title} />
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
-          <div className="movie-card-rating">
-            <Rating rating={rating} />
-          </div>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={rating} />
         </div>
       </div>
     );
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
-    const rating = this.props.rating;
+    const { rating } = this.props;
     return (
-      <div calssName="rating">
-        <p>{rating}</p>
+      <div className="rating">
+        <span>{rating}</span>
       </div>
     );
   }
