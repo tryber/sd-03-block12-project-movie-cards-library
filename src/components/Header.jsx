f97d7a6832c34d76081b6ac57849d05402049448
fabemiliano <fab.emiliano@gmail.com>
2020-04-24 14:45:47 -0300

    segundo requisito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return (<header>Header</header>);
+    return (<header><h1>Movie Cards Library</h1></header>);
   }
 }
 
