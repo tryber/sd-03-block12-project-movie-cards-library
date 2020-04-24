987fe2055ad026745907f33c92671a8e57cf0f35
William Igor <williamigorsilva@gmail.com>
2020-04-24 19:44:47 -0300

    Tags e classes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 // import MovieCard from './components/MovieCard'; <MovieCard />
-import dataMovies from './data';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movie={dataMovies} />
+      <MovieList movie={movies} />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,11 +5,11 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movies;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div className="movie-card">
         <div className="movie-card-body">
-          <img className="movie-card-image" src={imagePath} alt="" />
+          <img className="movie-card-image" src={imagePath} alt={title} />
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
