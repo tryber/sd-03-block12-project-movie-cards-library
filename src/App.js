7a99d91c4022eb7d0103ee4cb37f3bbe311bbe47
DiegoRafaelAF <draf0314@gmail.com>
2020-04-24 18:19:17 -0300

    Primeira avaliação Requisitos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import MovieCard from './data';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
-     
+      <div className='movie-card-header'>
+        <Header />
+      </div>
+      <div clasName='movie-list'>
+        <MovieList movie={MovieCard} />
+      </div>
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">
+        {this.props.movie.map((movies) => <MovieCard Key={movies.title} movie={movies} />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
