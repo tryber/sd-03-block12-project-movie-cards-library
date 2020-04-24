bbbda5c2f2287a7060a01a1e18104bcd44f07219
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-04-24 17:36:15 -0300

    Codeclimate check

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,6 @@
 // implement MovieCard component here
 import React from 'react';
-import Rating from './Rating'
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
