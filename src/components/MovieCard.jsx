67f0a941c3a703a984be781c978335ba67bfea54
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-04-24 15:04:59 -0300

    changes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import MovieList from './MovieList';
+import Rating from './Rating';
 
-export default function MovieCard() {
-  const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
-  return (
-    <>
-      <img src={imagePath} alt={title} />
-      <h4>
-        {title}
-      </h4>
-      
-    </>
-  );
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <>
+        <img src={imagePath} alt={title} />
+        <h4>
+          {title}
+        </h4>
+        <h5>
+          {subtitle}
+        </h5>
+        <p>
+          {storyline}
+        </p>
+        <Rating rating={rating} />
+      </>
+    );
+  }
 }
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import MovieCard from './MovieCard';
 
-export default function MovieList() {
-  const { movies } = this.props.movies;
-  return (
-    movies.map((movie) => <MovieCard key={movie.title} />)
-  );
+class MovieList extends React.Component {
+  render() {
+    const movies = this.props.movies;
+
+    return (
+      movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)
+    );
+  }
 }
+
+export default MovieList;
