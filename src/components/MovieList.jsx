7b61197c9aedfe3496444dfbb3c1bde8514c9c86
Marco Barbosa <marco.meireles.b@gmail.com>
2020-04-24 15:16:21 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+  }
+}
+
+export default MovieList;
