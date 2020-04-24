33bca3d3c09287c9e74b6a204c98dd0b8ff1d31b
Marco Barbosa <marco.meireles.b@gmail.com>
2020-04-24 17:38:45 -0300

    Adicionado o elemento Avaliacao

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div><img src={imagePath} alt="Ow" /><br />
         <div><h4>{title}</h4>
           <h5>{subtitle}</h5>
           <p>{storyline}</p>
         </div>
+        <Rating rating={rating} />
       </div>
     );
   }
