fa6e480f155d157bc799dea8db71e94e0838fbe7
Hfreitas <hfreitas86@gmail.com>
2020-04-24 14:58:09 -0300

    implementado componente header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,14 @@
 // implement Header component here
-//Iniciando
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Header>
+        <h1>Movie Cards Library</h1>
+      </Header>
+    );
+  }
+}
+
+export default Header;
