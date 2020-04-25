682f0c118880da2d6efb8a6b3b49f0c76c219bb9
henrique <hbaetaleite@gmail.com>
2020-04-24 21:19:23 -0300

    CodeClimate v4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,10 +7,12 @@ export default class MovieCard extends Component {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
       <div className="movie-card">
-        <h4 className="movie-card-title">{title}</h4>
-        <h5 className="movie-card-subtitle">{subtitle}</h5>
-        <img className="movie-card-image" src={imagePath} alt={title}/>
-        <p className="movie-card-storyline">{storyline}</p>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <img className="movie-card-image" src={imagePath} alt={title} />
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
         <div className="movie-card-rating">
           <Rating rating={rating} />
         </div>
