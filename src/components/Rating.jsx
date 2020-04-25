db5fd381b11c4e403ede3a89cb0cff07ba109be2
potew <potew0@gmail.com>
2020-04-25 10:50:53 -0300

    Pequena mudança no layout

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 class Rating extends React.Component {
   render() {
     return (
-      <div className="movie-card-rating">
+      <div className="movie-card-rating"> 
+        <p>Nota:</p> 
         <p className="rating">
           {this.props.rating}
         </p>
