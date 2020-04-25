0fe52b108d7d320c435d720cbd97160cd828e7f2
potew <potew0@gmail.com>
2020-04-25 09:52:38 -0300

    Final push

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 class Rating extends React.Component {
   render() {
     return (
-      <div className='movie-card-rating'>
-        <p className='rating'>
+      <div className="movie-card-rating">
+        <p className="rating">
           {this.props.rating}
         </p>
       </div>
