b149438bfaed60c9ef38c838d0f909f018453f60
rogerfaustino <roger_faustino@hotmail.com>
2020-04-27 11:06:33 -0300

    Codeclimate issues

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Rating from './Rating';
+// import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const {title, subtitle, storyline, imagePath , rating} = this.props.movie;
+    const { title, subtitle, storyline, imagePath } = this.props.movie;
     return (
       <div className="movie-card">
         <img
@@ -16,8 +16,7 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
-          </div>
-        {/* <Rating rating={rating} /> */}
+        </div>
       </div>
     );
   }
