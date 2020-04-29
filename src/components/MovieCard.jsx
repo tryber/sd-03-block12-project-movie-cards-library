3c3c09df386200908f975e47880eedd3f69bf91e
mirellaalves <mirella.alves@gmail.com>
2020-04-29 20:09:47 -0300

    Working in MovieCard - requirement 6.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+    render () {
+      const {title, subtitle, storyline, imagePath, rating} = this.props.movie;
+      return (
+        <div class='movie'>
+          {this.props.movie};
+        </div>
+      )
+    }
+}
+
+export default MovieCard;
