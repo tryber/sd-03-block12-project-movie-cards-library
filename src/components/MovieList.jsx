6d0a1c4187642ee9a4ba37977265a7b372f519ae
renan.andreolle <renandreolle@hotmail.com>
2020-04-29 18:04:17 -0300

    importei componentes2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,6 @@
 // implement MovieList component here
 import React from 'react';
-import MovieCard from './MovieCard'
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
