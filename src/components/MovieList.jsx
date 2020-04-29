68226dac7394062ee0c63d9c72403025ecf37ac4
renan.andreolle <renandreolle@hotmail.com>
2020-04-29 18:42:30 -0300

    briga contra o CC

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,6 @@
 // implement MovieList component here
 import React from 'react';
 import MovieCard from './MovieCard';
-//import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
