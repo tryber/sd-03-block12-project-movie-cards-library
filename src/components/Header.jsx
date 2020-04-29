9bd04f8f8633a037974f6027dc9fe1115379a4d3
mirellaalves <mirella.alves@gmail.com>
2020-04-29 18:34:29 -0300

    Code climate issues.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 class Header extends React.Component {
-    render () {
-        return (
-            <header></header>
-        );
-    }
+  render() {
+    return (
+      <header>Header</header>
+    );
+  }
 }
 
 export default Header;
