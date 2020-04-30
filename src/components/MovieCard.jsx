4bf6e3f372e64588e6a08bdfe17351ea67c25af6
Lucas Franca <luc.rangel90@gmail.com>
2020-04-30 11:54:44 -0300

    alguns requisitos feitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
-// implement MovieCard component here
+import React from 'react';
+
+// const MovieList = () => (
+//   const { title, subtitle, storyline, rating, imagePath } = this.PaymentResponse.movies;
+
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// );
+
+export default MovieList;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './components/MovieCard';
+
+const MovieList = () => (
+  <div>
+    {this.props.movies.map((movie) => (
+      <MovieCard key={movie.title} movie={movie} />
+    ))}
+  </div>
+);
+
+export default MovieList;
