ef0682539dfbbb34480713d001617451a5a2eea9
mirellaalves <mirella.alves@gmail.com>
2020-04-29 21:29:52 -0300

    Unchanging...

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,9 +4,9 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    {this.props.movies}
     return (
       <div className="movies">
+        {this.props.movies}
         {movies.map((movie) => <MovieCard movie={movie} />)}
       </div>
     );
