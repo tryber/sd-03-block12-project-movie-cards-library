0c94ba389456b8c6340211faf52371555cc3e6f4
Mateus Moreira <ashuraw@hotmail.com>
2020-04-30 17:40:57 -0300

    Codeclimate tentativa 1

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,13 +5,13 @@ export default class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <div className='movie-card'>
-        <img src={imagePath} className='movie-card-image' alt="" />
-        <div className='movie-card-body'>
-        <h4 className='movie-card-title'>{title}</h4>
-        <h5 className='movie-card-subtitle'>{subtitle}</h5>
-        <p className='movie-card-storyline'>{storyline}</p>
-        <Rating rating={rating} className='movie-card-rating' />
+      <div className="movie-card">
+        <img src={imagePath} className="movie-card-image" alt="" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={rating} className="movie-card-rating" />
         </div>
       </div>
     );
