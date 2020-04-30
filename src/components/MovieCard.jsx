017d5b14c1e60f7895132529b16b52edd86710d9
mirellaalves <mirella.alves@gmail.com>
2020-04-30 17:19:30 -0300

    Including classes for style.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,14 +5,16 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <div>
-        <div className="movie">
-          <img src={imagePath} alt={title} />
-          <h4>{title}</h4>
-          <h5>{subtitle}</h5>
-          <p>{storyline}</p>
+      <div className="movie-card">
+        <img className="movie-card-image" src={imagePath} alt={title} />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
         </div>
-        <Rating rating={rating} />
+          <div className="movie-card-rating">
+            <Rating rating={rating} />
+          </div>
       </div>
     );
   }
