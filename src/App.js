43e7c5487cd3bad8ed15b1564a800f2cd985b752
Rafaopb <59584187+Rafaopb@users.noreply.github.com>
2020-04-30 17:53:49 -0300

    Renderizando Movie Data

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 import Header from './components/Header';
 
 import MovieList from './components/MovieList';
+import movie from './data'; 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={movies} />
+      <MovieList movies={movie} />
     </div>
   );
 }
