00b3bd0fb330e9479295598c8c4d02abf2e4de41
Rafaopb <59584187+Rafaopb@users.noreply.github.com>
2020-04-30 17:20:19 -0300

    App foi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ import '../App.css';
 class Header extends React.Component {
   render() {
     return (
-      <header className="movie-card-header"><h1>Movie Cards Library</h1></header>
+      <header className="movie-card-header">
+        <h1>Movie Cards Library</h1>
+      </header>
     );
   }
 }
