f388dec8289f596f20f1ad0ad3607d4fc9d12da0
Lucas Franca <luc.rangel90@gmail.com>
2020-04-30 12:01:55 -0300

    tentando novamente depois do erro no teste

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,12 @@
-// import React from 'react';
+import React from 'react';
 
 // const MovieList = () => (
-//   const { title, subtitle, storyline, rating, imagePath } = this.PaymentResponse.movies;
+//   const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
 
-//   return (
 //     <div>
 
 //     </div>
 //   )
 // );
 
-// export default MovieList;
+export default MovieList;
