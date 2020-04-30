7370c2365cec2d8dca6ec8b2efb50746166e4377
RobertoAsgon <60623542+RobertoAsgon@users.noreply.github.com>
2020-04-30 17:24:17 -0400

    codeclimate fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import moviesData from'./data';
+import moviesData from './data';
 import MovieList from './components/MovieList';
 
 
