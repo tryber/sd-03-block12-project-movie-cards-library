ce22c2bd6c456c036e8950f85d3c0e6bd902e432
tamagoshii <estelaap_martins@hotmail.com>
2020-05-05 20:02:42 -0300

    Validando

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <header />
+      <Header />
       <MovieList movies={movie} />
     </div>
   );
