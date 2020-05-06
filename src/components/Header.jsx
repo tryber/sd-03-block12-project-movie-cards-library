15f208782a82e4418fe8a7881c054e3b59db91a7
André Ferreira <andrefpires1@gmail.com>
2020-05-06 19:24:06 -0300

    Requisito 2 | Code Climate #2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-  class Header extends React.Component {
-    render() {
-      return (
-        <div className="movie-card-header">
-          <header>
-            <h1>Movie Cards Library</h1>
-          </header>
-        </div>
-      );
-    }
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="movie-card-header">
+        <header>
+          <h1>Movie Cards Library</h1>
+        </header>
+      </div>
+    );
   }
+}
 
 export default Header;
