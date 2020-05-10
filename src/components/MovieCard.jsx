9c8437584388c3ccbb7f3f4b3915e4e63cd39871
Gabriel Oliveira Ferreira <gabriel1335555@gmail.com>
2020-05-10 01:54:00 -0300

    codeclimate9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,10 +7,18 @@ export default class MovieCard extends React.Component {
     const dataMovies = this.props.movies;
     return (
       <section>
-        {dataMovies.map(
-          ({ title, subtitle, storyline, rating, imagePath }) =>
-            <MovieList key={title} title={title} subtitle={subtitle} 
-            storyline={storyline} rating={rating} imagePath={imagePath} />)}
+        {
+          dataMovies
+          .map(
+            ({ title, subtitle, storyline, rating, imagePath }) => 
+              <MovieList 
+                key={title}
+                title={title}
+                subtitle={subtitle}
+                storyline={storyline}
+                rating={rating}
+                imagePath={imagePath} 
+              />)}
       </section>
     );
   }
