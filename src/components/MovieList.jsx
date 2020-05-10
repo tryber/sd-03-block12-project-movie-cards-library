bba0d1d2dedf9b6511d3c92d91c11054cce48a65
Gabriel Oliveira Ferreira <gabriel1335555@gmail.com>
2020-05-10 02:55:41 -0300

    codeclimate15

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,8 +5,7 @@ import MovieCard from './MovieCard';
 export default class MovieList extends React.Component {
   render() {
     const dataMovies = this.props.movies;
-    return (dataMovies
-      .map(movie => <MovieCard key={movie.title} movie={movie} />)
-    );
+    return dataMovies
+      .map((movie) => <MovieCard key={movie.title} movie={movie} />);
   }
 }
